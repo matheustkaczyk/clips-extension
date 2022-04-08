052314569ff97de311fb7a1b20a0c6036ad51d98
matheustkaczyk <matheus111904@hotmail.com>
2022-04-08 14:46:40 -0300

    APlicando lib clipboard, aplicando-a no componente card, tentando arrumar css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import copy from 'copy-to-clipboard';
 import { Button } from './Button';
 
 export const Card = ({ name, content, id }) => {
   return(
-    <section>
+    <section onClick={ () => copy(content) } className="card">
       <h3>{name}</h3>
       <Button type="button" placeholder={ 'Excluir' } />
     </section>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,8 @@ import { Card } from './Card';
 
 export const Main = ({ storage }) => {
   return(
-    <div>
-      {storage.length > 0 && storage.map((item) => <Card name={item.name} content={item.content} id={item.id} />)}
+    <div className="card-wrapper">
+      {storage.length > 0 && storage.map((item) => <Card key={item.id} name={item.name} content={item.content} id={item.id} />)}
     </div>
   )
 };
