4fa21652cbcd2458b1bd8e1c55ae75aa449d5c88
matheustkaczyk <matheus111904@hotmail.com>
2022-04-08 10:28:31 -0300

    zerando padding e margin no index css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { AiOutlinePaperClip } from 'react-icons/ai';
 
 export const Header = () => {
   return (
-    <h2>Header</h2>
+    <header>
+      <AiOutlinePaperClip/>
+      <h2>Clips</h2>
+    </header>
   )
 };
