9edb4091130883f4786422ac567678c9b29fe9f4
matheustkaczyk <matheus111904@hotmail.com>
2022-04-09 15:31:31 -0300

    inb4 mudança de visual

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Info = () => {
   return(
     <div className="info">
-      <h4>Add your first item</h4>
+      <h4>Add your shortcuts</h4>
     </div>
   )
 };
